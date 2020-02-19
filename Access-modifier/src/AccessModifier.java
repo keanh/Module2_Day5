8f881562aead831122cddb9bd772bdc601027a25
keanh <keanh275@gmail.com>
2020-02-19 17:22:39 +0700

    Add

---
+++ Access-modifier/src/AccessModifier.java
@@ -0,0 +1,22 @@
+//
+//public class AccessModifier {
+//
+//    private int data = 40;
+//
+//    private void msg() {
+//        System.out.println("Hello java");
+//    }
+//
+//
+//}
+//public class Simple{
+//
+//        public static void main(String args[]){
+//            AccessModifier obj = new AccessModifier();
+//
+//            System.out.println(obj.data);//Compile Time Error
+//
+//            obj.msg();//Compile Time
+//        }
+//
+//    }
